59096ab44a281e13e6a54f019b86fc1aa113921c
“Sahil <ss2202002@gmail.com>
2024-03-06 14:24:19 +0530

    fixed importing issues

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { createRoot } from 'react-dom/client';
 import { Provider } from "react-redux";
-const container = document.getElementById('root');
 import store from "./redux/store";
+const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
 root.render(
