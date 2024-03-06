8362b51de4a914fcd1bc6d710b9256ec5082289d
“Sahil <ss2202002@gmail.com>
2024-03-06 15:10:38 +0530

    made .env files for safety

--- src/services/CryptoNewsApi.js
+++ src/services/CryptoNewsApi.js
@@ -1,17 +1,15 @@
 import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const cryptoNewsHeader = {
-  'x-api-key': 'b12dc4b686b94bf0af9fc425544ea175',
-}
+
 const baseUrl = 'https://newsapi.org/v2/everything';
-const createRequest = (url)=>({url,headers:cryptoNewsHeader});
+const createRequest = (url)=>({url});
 
 export const cryptoNewsApi = createApi({
   reducerPath: 'cryptoNewsApi',
   baseQuery: fetchBaseQuery({baseUrl}),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
-      query:({count})=>createRequest(`?q=crypto&pageSize=${count}`),
+      query:({count})=>createRequest(`?q=crypto&pageSize=${count}&apikey=${process.env.REACT_APP_API_KEY}`),
     })
   })
 });
