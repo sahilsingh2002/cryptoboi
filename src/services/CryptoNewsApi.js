1f97989c813d279a9d8c9204ae8d6c7071f3956d
“Sahil <ss2202002@gmail.com>
2024-03-11 16:40:39 +0530

    used API from CryptoNews

--- src/services/CryptoNewsApi.js
+++ src/services/CryptoNewsApi.js
@@ -3,13 +3,14 @@ import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl = 'https://newsapi.org/v2/everything';
 const createRequest = (url)=>({url});
+const REACT_APP_API_KEY = "b12dc4b686b94bf0af9fc425544ea175"
 
 export const cryptoNewsApi = createApi({
   reducerPath: 'cryptoNewsApi',
   baseQuery: fetchBaseQuery({baseUrl}),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
-      query:({count})=>createRequest(`?q=crypto&pageSize=${count}&apikey=${process.env.REACT_APP_API_KEY}`),
+      query:({count})=>createRequest(`?q=crypto&pageSize=${count}&apikey=${REACT_APP_API_KEY}`),
     })
   })
 });
